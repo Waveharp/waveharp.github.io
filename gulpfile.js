85d4ea6bed330e5be758f689a802b77f8ae3020b
Josh Stricklin <jostricklin@gmail.com>
2016-04-11 13:30:17 -0500

    rearrange directory, congigure Lost CSS, change gulp.watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,40 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-var postcss = require('gulp-postcss');
+var gulp 				= require('gulp');
+var browserSync = require('browser-sync').create();
+var postcss 		= require('gulp-postcss');
+var sourcemaps  = require('gulp-sourcemaps');
+var lost 				= require('lost');
+
+var reload 			= browserSync.reload;
+
+var paths = {
+	cssSource: 'src/css/',
+	cssDestination: 'dist/css/'
+};
+
+// sourcemaps, lost
+gulp.task('styles', function() {
+	return gulp.src(paths.cssSource + '**/*.css')
+		.pipe(sourcemaps.init())
+		.pipe(postcss([
+			lost()
+		]))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(paths.cssDestination));
+});
+
+gulp.watch(paths.cssSource + '**/*.css', ['styles']);
+
+gulp.task('default', ['styles']);
 
 // watch files for changes and reload
-gulp.task('serve', function() {
-	browserSync({
-		server: {
-			baseDir: '.'
-		}
-	});
+// gulp.task('serve', function() {
+// 	browserSync({
+// 		server: {
+// 			baseDir: './src'
+// 		}
+// 	});
 
-	gulp.watch(['*.html', 'css/*.css'], {cwd: '.'}, reload);
-});
+// 	gulp.watch(['*.html', 'css/*.css'], {cwd: './src'}, reload);
+// });
 
-gulp.task("default", ["serve"], function() {
-	console.log("Watching files");
-});
+gulp.task("default", ["styles"]);
---
+++ readme.md
@@ -0,0 +1,7 @@
+### To Do
+- much the same as the Boardy framework, minus the Redux stuff
+- [x] install Lost
+- [ ] install Rucksack
+	- [ ] and autoprefixer
+- [ ] install precss
+- [ ] configure browser-sync
